279f6dccc4716826e801b08040696345e8b477a5
Spretzsanti <spretzsanti@gmail.com>
2025-05-22 08:59:29 -0300

    Arbol AVL metodos de insercion

--- TrabajoPractico_2/proyecto_2/modules/AVL.py
+++ TrabajoPractico_2/proyecto_2/modules/AVL.py
@@ -42,7 +42,7 @@ class AVL:
 
         return nodo.altura
 
-    def factor_eq(self): #agregar validaciones
+    def factor_eq(self,nodo): #agregar validaciones
           return self._altura(nodo.izquierda) - self._altura(nodo.derecha)      
 
     def actualizar_altura(self,nodo): #actualizamos la altura de los nodos post rotaciones, a partir de la altura de sus hijos izq y der
@@ -86,6 +86,63 @@ class AVL:
 
     def rot_der_izq(self,z):
          z.derecha = self.rot_derecha(z.derecha)
+
+
+    #Metodos del propio arbol
+    def insertar(self,nodo_actual,fecha:datetime.date, temperatura:float):
+
+        #Inserciones normales
+        if not nodo_actual:
+            return nodoAVL(fecha,temperatura)
+
+        elif fecha > nodo_actual.fecha:
+            nodo_actual.derecha = self.insertar(nodo_actual.derecha,fecha,temperatura) 
+        
+        elif fecha < nodo_actual.fecha:
+            nodo_actual.izquierda = self.insertar(nodo_actual.izquierda,fecha,temperatura) 
+        else:
+            #Esto es para las fechas duplicadas, actualizamos el valor de la temperatura
+            nodo_actual.temperatura = temperatura
+            return nodo_actual
+
+
+
+        #Paso 2
+        self.actualizar_altura(nodo_actual)
+        #Obtenemos FE
+        balance = factor_eq(nodo_actual)
+
+        
+        #AHorad dependiendo del valor del factor de equolibrio hacemos las rotaciones correspondiente
+        #Dos a la izq
+        #El nodo está cargado a la izquierda (balance > 1) y se inserto en el subárbol izquierdo de su hijo izquierdo
+        if balance > 1 and fecha < nodo_actual.izquierda.fecha:
+            return self.rot_derecha(nodo_actual)
+
+        #Dos a la derecha
+        if balance < -1 and fecha > nodo_actual.izquierda.fecha:
+            return self.rot_izquierda(nodo_actual)
+
+        #Izquierda y derecha
+        if balance > 1 and fecha > nodo_actual.izquierda.fecha:
+            nodo_actual.izquierda = self.rot_izquierda(nodo_actual.izquierda)
+            return self.rot_derecha(nodo_actual)
+        #Derecha y izquierda
+        if balance < -1 and fecha < nodo_actual.izquierda.fecha:
+            nodo_actual.derecha = self.rot_derecha(nodo_actual.derecha)
+            return self.rot_izquierda(nodo_actual)
+
+        return nodo_actual #Si estaba bien
+
+
+
+
+        
+
+
+
+
+
 
  
 
---
+++ TrabajoPractico_2/proyecto_2/modules/temperatura_db.py
@@ -0,0 +1,13 @@
+from AVL.py import AVL
+from AVL.py import nodoAVL
+
+
+
+class temperatura_db:
+    def __init__(self):
+        self.arbol_temperatura = AVL() #Creamos la instancia en si
+
+    def guardar_temperatura(temperatura,fecha):
+        
+
+
